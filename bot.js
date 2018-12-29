407c7ef7f41a52a58620a0895d6ecc66d80f0175
Matrks <45499570+Matrks@users.noreply.github.com>
2018-12-29 22:35:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -397,34 +397,125 @@ message.channel.sendEmbed(embed)
 }
 });
 
-
-
-client.on("message", message => {//Toxic Codes
-    var prefix = "-";// البرفكس
-    if(message.content.startsWith(prefix + "setwlc")) {//Toxic Codes
-        let args = message.mentions.channels.first();
-            if(!args) message.channel.send("** منشن روم . ❌**").then(m => { //Toxic Codes   
-m.delete(1500);
-})
-                if(!message.guild.member(message.author.id).hasPermission("MANAGE_CHANNELS")) return message.channel.send("**ليس لديك صلاحيات . ❌**");
-                        message.channel.send(`**${args}. لقد تم شغل الروم هذا للترحيب.**`);//Toxic Codes
-                    client.on("guildMemberAdd", (member) => {//Toxic Codes
-                            if(member.user.bot) return;//Toxic Codes
-                         var embed = new Discord.RichEmbed()//Toxic Codes
-    .setAuthor(member.user.username, member.user.avatarURL)//Toxic Codes
-    .setThumbnail(member.user.avatarURL)//Toxic Codes
-    .setTitle('New Member')
-    .setDescription('Welcome To Server')//Toxic Codes
-    .addField('**ID Member:',"" +  member.user.id, true)//Toxic Codes
-    .addField('**Tage Member:', member.user.discriminator, true)//Toxic Codes
-    .addField('Created At Member', member.user.createdAt, true)//Toxic Codes
-    .addField(' 👤  Your Number',`**[ ${member.guild.memberCount} ]**`,true)//Toxic Codes
-    .setColor('GREEN')
-    .setFooter(member.guild.name, member.guild.iconURL, true)
-                         
-   args.send({embed : embed});//Toxic Codes
-                    });
-    }
+const sWlc = {}
+client.on('message', message => {
+var prefix = "-";
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+    channel: "welcome"
+}
+const channel = sWlc[message.guild.id].channel
+  if (message.content.startsWith(prefix + "setwelcomer")) {
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newChannel = message.content.split(' ').slice(1).join(" ")
+    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
+    sWlc[message.guild.id].channel = newChannel
+    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
+  }
+});
+ 
+client.on("guildMemberAdd", member => {
+      if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
+    channel: "welcome"
+  }
+  const channel = sWlc[member.guild.id].channel
+    const sChannel = sWlc[member.guild.id].channel
+    let welcomer = member.guild.channels.find('name', sChannel);
+    let memberavatar = member.user.avatarURL
+      if (!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let heroo = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:heroo});          
+         
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+     
+    const w = ['./w1.png'];
+ 
+              let Image = Canvas.Image,
+                  canvas = new Canvas(400, 200),
+                  ctx = canvas.getContext('2d');
+ 
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 400, 200);
+ 
+      })
+ 
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+ 
+                                    ctx.font = "bold 12px Arial";
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#f1f1f1";
+                                ctx.fillText(member.user.username, 200, 150);
+ 
+                              //NAMEً
+                              ctx.font = "bold 12px Arial";
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#f1f1f1";
+      ctx.fillText(`Welcome To Server`, 220, 125);
+ 
+                              //AVATARً
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(77, 101, 62, 0, Math.PI*2);
+                              ctx.stroke();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 13, 38, 128, 126);
+ 
+ 
+    welcomer.sendFile(canvas.toBuffer())
+ 
+ 
+ 
+      })
+      })
+ 
+      }
+      });
+   
+   
+const invites = {};
+ 
+const wait = require('util').promisify(setTimeout);
+ 
+client.on('ready', () => {
+  wait(1000);
+ 
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+ 
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "welcome");
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+  });
 });
 
 client.login(process.env.BOT_TOKEN);
